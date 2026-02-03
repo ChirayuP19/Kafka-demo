1d10d07fa3be1f7972e0f41fc50e6a66f6627663
ChirayuP19 <chirayu010675@gmail.com>
2026-02-03 09:30:16 -0500

    Stream update to Consumer.

--- consumer (1)/consumer/src/main/java/com/example/consumer/KafkaConsumer.java
+++ consumer (1)/consumer/src/main/java/com/example/consumer/KafkaConsumer.java
@@ -1,19 +1,19 @@
 package com.example.consumer;
 
-import com.example.consumer.rider.RiderLocation;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
-
-@Component
-public class KafkaConsumer {
-
-    @KafkaListener(topics = "my-topic",groupId = "my-new-group")
-    public void listen(String message){
-        System.out.println("Received Message 1: "+message);
-    }
-
-    @KafkaListener(topics = "my-new-topics-2",groupId = "my-new-group-rider")
-    public void listenRiderLocation(RiderLocation location){
-        System.out.println("Received location: "+location.getRiderID()+" : "+location.getLatitude()+": "+location.getLongitude());
-    }
-}
+//import com.example.consumer.rider.RiderLocation;
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class KafkaConsumer {
+//
+//    @KafkaListener(topics = "my-topic",groupId = "my-new-group")
+//    public void listen(String message){
+//        System.out.println("Received Message 1: "+message);
+//    }
+//
+//    @KafkaListener(topics = "my-new-topics-2",groupId = "my-new-group-rider")
+//    public void listenRiderLocation(RiderLocation location){
+//        System.out.println("Received location: "+location.getRiderID()+" : "+location.getLatitude()+": "+location.getLongitude());
+//    }
+//}
---
+++ consumer (1)/consumer/src/main/java/com/example/consumer/KafkaConsumerNew.java
@@ -0,0 +1,18 @@
+package com.example.consumer;
+
+
+import com.example.consumer.rider.RiderLocation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Consumer;
+
+@Configuration
+public class KafkaConsumerNew{
+
+    @Bean
+   public Consumer<RiderLocation> processRiderLocation(){
+        return location-> System.out.println("Received: "+location.getRiderID()+": "+location.getLatitude()+", "+location.getLongitude());
+    }
+
+}
