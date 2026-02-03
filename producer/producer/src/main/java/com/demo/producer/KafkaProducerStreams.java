1ebbe8019cfc6cdfb2eb6f52b6a09c9a4d3d04a1
ChirayuP19 <chirayu010675@gmail.com>
2026-02-03 09:44:26 -0500

    Update Rider ID generator

--- producer/producer/src/main/java/com/demo/producer/KafkaProducerStreams.java
+++ producer/producer/src/main/java/com/demo/producer/KafkaProducerStreams.java
@@ -4,6 +4,7 @@ import com.demo.producer.rider.RiderLocation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Random;
 import java.util.function.Supplier;
 
 @Configuration
@@ -11,8 +12,10 @@ public class KafkaProducerStreams {
 
     @Bean
     public Supplier<RiderLocation> sendRiderLocation(){
+        Random random = new Random();
         return ()->{
-            RiderLocation location = new RiderLocation("Rider123", 16.7, 98.45);
+            String riderID= "rider" +random.nextInt(20);
+            RiderLocation location = new RiderLocation(riderID, 16.7, 98.45);
             System.out.println("Sending: "+location.getRiderID());
             return location;
         };
