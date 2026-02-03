41ea9763e26df76b1dd3d4b74a22745c9b629332
ChirayuP19 <chirayu010675@gmail.com>
2026-02-03 10:03:27 -0500

    Supplier send message to Different topics.

--- producer/producer/src/main/java/com/demo/producer/KafkaProducerStreams.java
+++ producer/producer/src/main/java/com/demo/producer/KafkaProducerStreams.java
@@ -3,6 +3,12 @@ package com.demo.producer;
 import com.demo.producer.rider.RiderLocation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.util.MimeTypeUtils;
+
 
 import java.util.Random;
 import java.util.function.Supplier;
@@ -18,6 +24,20 @@ public class KafkaProducerStreams {
             RiderLocation location = new RiderLocation(riderID, 16.7, 98.45);
             System.out.println("Sending: "+location.getRiderID());
             return location;
+        };
+    }
+
+    @Bean
+    public Supplier<Message<String>> sendRiderStatus(){
+        Random random = new Random();
+        return ()->{
+            String riderID= "rider" +random.nextInt(20);
+            String status = random.nextBoolean() ? "rider started" : "ride completed";
+            System.out.println("Sending: "+status);
+            return MessageBuilder.withPayload(riderID+" :status "+status)
+                    .setHeader(KafkaHeaders.KEY,riderID.getBytes())
+                    .setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.TEXT_PLAIN)
+                    .build();
         };
     }
 }
